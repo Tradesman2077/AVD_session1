f0204b53b0c88731cb0368e5e1fa6e227f968014
christopher wright <chriswright21@live.co.uk>
2022-03-30 01:28:13 +0100

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,37 +1,92 @@
-let x = 0;
+//const density = "Ñ@#W$9876543210?!abc;:+=-,._                    ";
+let density = '   .:-i0|ze=+X%';
+//const density = '   .:-i|=+HENRY';
+
+let video;
+let asciiDiv;
+let counter=0;
+let fonts = ['Courier', 'Inconsolata Mono', 'Courier New'];
+
 
 function setup() {
-  createCanvas(800, 800);
-  
+  video = createVideo("unChien.mp4");
+  noCanvas();
+  //video = createCapture(VIDEO);
 
+  video.size(150, 75);
+  
+  asciiDiv = createDiv();
+  frameRate(24);
+}
+function switchFont(){
+  console.log('switch');
+  document.getElementById('content').style.fontFamily = fonts[Math.floor(Math.random()*fonts.length)];
+}
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+function switchDensity(){
+  if(density == '   .:-i|ze=+X%'){
+    density = "Ñ@#W$9876543210?!abc;:+=-,._                    ";
+    document.getElementById('content').style.backgroundColor = 'black';
+    document.getElementById('content').style.color = 'red';
+  }
+  else{
+    density ='   .:-i|ze=+X%';
+    document.getElementById('content').style.backgroundColor = 'black';
+    document.getElementById('content').style.color = 'green';
+  }
+}
+function mouseMoved(){
+  video.play()
 }
 
 function draw() {
-  background(150);
-  
-  frameRate(8)
-  strokeWeight(0)
-  fill(Math.random()* 254, Math.random()* 254, Math.random()* 254)
+  video.size(150, 75);
+  video.loadPixels();
+  let asciiImage = "";
+  document.getElementById('content').style.fontSize = '12pt';
+  for (let j = 0; j < video.height; j++) {
 
-  rect(Math.random()* 400, 0, 55, 55);
-  rect(Math.random()* 400, 100, 55, 55);
-  rect(Math.random()* 400, 200, 155, 55);
-  rect(Math.random()* 400, 300, 255, 55);
-  fill(Math.random()* 254, Math.random()* 254, Math.random()* 254)
-  rect(Math.random()* 400, 400, 355, 55);
-  rect(Math.random()* 400, 500, 455, 55);
-  rect(Math.random()* 400, 600, 555, 55);
-  rect(Math.random()* 400, 700, 655, 55);
-  
-  ellipse(650, 100, Math.random()*300, Math.random()*150);
-  fill(Math.random()* 254, Math.random()* 254, Math.random()* 254, 125);
-  rect(0,0,x,800);
-  rect(800,800, 800, x);
-  if(x < 800){
-    x+=10;
-  }
-  else{
-    x = 0;
-  }
+    for (let i = 0; i < video.width; i++) {
 
+      let rand = Math.floor(Math.random()*100000);
+      if(rand == 2){
+        break;
+      }
+      else if(rand == 8){
+        for(let k = 0; k < video.width; k++){
+          let size=  randomIntFromInterval(0, 500).toString();
+          document.getElementById('content').style.fontSize = size+ "pt";
+          asciiImage+="X";
+          
+        } 
+      }
+      else if(rand == 100){
+        video.size(600, 300);
+      }
+      const pixelIndex = (i + j * video.width) * 4;
+      const r = video.pixels[pixelIndex + 0];
+      const g = video.pixels[pixelIndex + 1];
+      const b = video.pixels[pixelIndex + 2];
+      const avg = (r + g + b) / 3;
+      const len = density.length;
+      const charIndex = floor(map(avg, 0, 255, 0, len));
+      const c = density.charAt(charIndex);
+      if (c == " ") asciiImage += "&nbsp;";
+      else asciiImage += c;
+    }
+    //add line break
+    asciiImage += '<br/>';
+  }
+  counter++;
+  console.log(counter);
+  if(counter%50 == 0){
+    switchFont();
+  }
+  if(counter > 100){
+    counter = 0;
+    switchDensity();
+  }
+  asciiDiv.html(asciiImage);
 }
